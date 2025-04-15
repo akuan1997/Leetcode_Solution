2d2de87fc624539c19ff427a5c44d1a24cb4894f
akuan10 <akuan10cc@gmail.com>
2025-04-15 13:23:26 +0800

    -

---
+++ 201-300/219. Contains Duplicate II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hash_map = {}
+        for i, num in enumerate(nums):
+            if num in hash_map:
+                if abs(i - hash_map[num]) <= k:
+                    return True
+            hash_map[num] = i
+        return False
